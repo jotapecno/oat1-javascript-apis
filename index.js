078157c6ec835e381e50b4d958c38afd32f0e409
João Paulo Carneiro <jotapecno@gmail.com>
2026-09-19 03:26:48 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Executa a demonstração de todas as 10 questões em sequência.
+ * Rode com: node index.js
+ */
+require("./src/q1-contagem-regressiva.js").demo();
+require("./src/q2-calculadora-imc.js").demo();
+require("./src/q3-tabuada.js").demo();
+require("./src/q4-validador-usuario.js").demo();
+require("./src/q5-analisador-numeros.js").demo();
+require("./src/q6-conversor-temperatura.js").demo();
+require("./src/q7-contador-caracteres.js").demo();
+require("./src/q8-gerador-token.js").demo();
+require("./src/q9-maior-palavra.js").demo();
+require("./src/q10-distribuir-times.js").demo();
